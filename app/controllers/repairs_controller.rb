1267af1e991dfb5ca1f3313bbb490dfb2039f19e
Yutaka Yanoh <yanoh@users.sourceforge.net>
2014-09-04 11:45:27 +0900

    未請求一覧画面を追加完了

--- app/controllers/repairs_controller.rb
+++ app/controllers/repairs_controller.rb
@@ -1,7 +1,7 @@
 class RepairsController < ApplicationController
   before_action :set_repair, only: [:show, :edit, :update, :destroy]
 
-  after_action :anchor!, only: [:index]
+  after_action :anchor!, only: [:index, :index_unbilled]
   after_action :keep_anchor!, only: [:show, :new, :edit, :create, :update, :engineArrived, :repairStarted, :repairFinished, :repairOrder]
 
   # GET /repairs
@@ -263,16 +263,33 @@ class RepairsController < ApplicationController
 
   # 未請求作業一覧を表示する
   def index_unbilled
-    # TODO: ダミー実装
-    @searched = Hash.new
-    session[:searched] = @searched
-    p :index_unbilled, params[:search]
-    @repairs = Repair.joins(:engine).where(
-      finish_date: (Date.parse("2014-08-26")..Date.parse("2014-09-25")),
-      engines: {enginestatus_id: Enginestatus.of_finished_repair}
-    ).paginate(page: params[:page], per_page: 10)
-    adjust_page(@repairs)
+    if params[:page]
+      # ページ繰り時は、検索条件を引き継ぐ
+      @searched = session[:searched]
+    else
+      if params[:commit]
+        # 再検索時は、以前の検索条件に新しく入力された条件をマージ
+        @searched = session[:searched]
+        @searched.merge!(params[:search])
+      else
+        # 初期表示時は、当月を検索条件として設定
+        @searched = {"billing_month(1i)" => Date.today.year,
+                     "billing_month(2i)" => Date.today.month}
+        session[:searched] = @searched
+      end
+    end
+
+    year  = @searched["billing_month(1i)"].to_i  # 請求月度 (年)
+    month = @searched["billing_month(2i)"].to_i  # 請求月度 (月)
+    end_date = Date.new(year, month, 25)  # TODO: 締め日を常数定義すること
+    start_date = end_date.advance(months: -1, days: 1)  # 前月の締め日の翌日
+
     # TODO: 整備ステータスに対応すること
+    @repairs = Repair.joins(:engine).where(
+      finish_date: start_date..end_date,
+      engines: {enginestatus_id: Enginestatus.of_finished_repair}
+    ).order(:finish_date).paginate(page: params[:page], per_page: 10)
+    adjust_page(@repairs)
   end
 
   private
