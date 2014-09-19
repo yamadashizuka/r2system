a0e3c751385aea0d68f616720dd922943003524d
unknown <EST00045@11D0914.es.ynet.local>
2014-09-19 13:57:40 +0900

    test

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,4 @@
+#encoding:utf-8
 class Company < ActiveRecord::Base
   has_many :engineorders
 
--- db/schema.rb
+++ db/schema.rb
@@ -139,9 +139,9 @@ ActiveRecord::Schema.define(version: 20140520083527) do
     t.string   "userid"
     t.string   "name"
     t.string   "category"
+    t.integer  "company_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "company_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
