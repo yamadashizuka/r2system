63757f822d73c37caec7ce1644745659c498455a
yamadashizuka <P0372979@P0372979B.ad.ogis-ri.co.jp>
2014-10-14 18:38:03 +0900

    Sprint11にあわせてテストコードを修正

--- test/acceptance/scenario_test.rb
+++ test/acceptance/scenario_test.rb
@@ -3,7 +3,7 @@
 require 'test_helper'
 
 class ScenarioTest < AcceptanceTest
-  fixtures :users, :companies, :enginestatuses, :businessstatuses
+  fixtures :users, :companies, :enginestatuses, :businessstatuses, :paymentstatuses
 
   # シナリオ１(sprint#2)
   test "scenario1 of release0_8" do
@@ -23,7 +23,7 @@ class ScenarioTest < AcceptanceTest
     click_link "新規登録"
     save_screenshot "scenario1-1_3.png"
     # 1.4. エンジンを登録する
-    fill_in "エンジン型式", with: "ENG0002"
+    fill_in "エンジン型式", with: "エンジンモデルコード2"
     fill_in "エンジンNo.", with: "SN0001"
     save_screenshot "scenario1-1_4_1.png"
     click_button "登録"
@@ -31,21 +31,24 @@ class ScenarioTest < AcceptanceTest
     save_screenshot "scenario1-1_4_2.png"
     # 1.5. エンジン一覧画面に戻る
     click_link "戻る"
-    assert has_content? "ENG0002"
+    assert has_content? "エンジンモデルコード2"
     save_screenshot "scenario1-1_5.png"
     # 1.6. 受領画面を開く
-    click_link "受領"
+    within "tbody > tr:nth-of-type(6)" do
+      click_link "受領"  # エンジン一覧の6行目にある受領ボタンをクリック
+    end
     # 受領時は受領日の登録のみ
     # 受領日のデフォルトは当日日付なので通常はボタン押下のみ
     save_screenshot "scenario1-1_6_1.png"
     click_button "受領登録"
     confirm
     save_screenshot "scenario1-1_6_2.png"
-    # 1.7. 整備作業一覧画面に移動する
+    # 1.7. 整備会社作業一覧画面に移動する
     click_link "戻る"
-    within "body > div > table > tbody > tr" do
+    #登録したエンジンが一覧にあることを確認する
+    within "tbody > tr:nth-of-type(6)" do
       assert_equal "整備前",  nth_tag(:td, 1).text # ステータス
-      assert_equal "ENG0002", nth_tag(:td, 2).text # エンジン型式
+      assert_equal "エンジンモデルコード2", nth_tag(:td, 2).text # エンジン型式
       assert_equal "SN0001",  nth_tag(:td, 3).text # エンジンNo.
     end
     save_screenshot "scenario1-1_7.png"
@@ -61,7 +64,7 @@ class ScenarioTest < AcceptanceTest
     # 2.1. サインインする
     sign_in "AA000001", "password"
     save_screenshot "scenario1-2_1.png"
-    # 2.2. 整備作業一覧画面を開く
+    # 2.2. 整備会社作業一覧画面を開く
     click_link "整備会社作業一覧"
     save_screenshot "scenario1-2_2.png"
     # 2.3. 整備依頼画面を開く
@@ -75,13 +78,25 @@ class ScenarioTest < AcceptanceTest
     click_button "依頼"
     confirm
     save_screenshot "scenario1-2_3_3.png"
-    # 2.4. 整備作業一覧画面に移動する
+    # 2.4. 整備会社作業一覧画面に移動する
     click_link "戻る"
+    save_screenshot "scenario1-2_3_3_2.png"
+ 
+    #整備依頼をしたエンジンを一覧上で確認する
+    #※データが多のでエンジン型式とエンジンNo.で検索する
+    select "エンジンモデルコード2", from: "search_engine_model_name"
+    fill_in "search_serialno", with: "SN0001"
+    click_button "検索"
+
+    #整備依頼をしたエンジンの完成希望日とステータスが
+    #入力通りに登録されていることを確認
+    save_screenshot "scenario1-2_3_3_3.png"
     within "tbody > tr" do
-      assert_equal "14/01/15", nth_tag(:td, 4).text # 完成希望日
+      assert_equal "2014/01/15", nth_tag(:td, 4).text # 完成希望日
       assert_equal "整備前", nth_tag(:td, 1).text   # ステータス
     end
     save_screenshot "scenario1-2_4.png"
+
     # 2.5. サインアウトする
     sign_out
     save_screenshot "scenario1-2_5.png"
@@ -94,9 +109,15 @@ class ScenarioTest < AcceptanceTest
     # 3.1. サインインする
     sign_in "SG000001", "password"
     save_screenshot "scenario1-3_1.png"
-    # 3.2. 整備作業一覧画面を開く
+    # 3.2. 整備会社作業一覧画面を開く
     click_link "整備会社作業一覧"
     save_screenshot "scenario1-3_2.png"
+    #※データが多のでエンジン型式とエンジンNo.で検索する
+    select "エンジンモデルコード2", from: "search_engine_model_name"
+    fill_in "search_serialno", with: "SN0001"
+    click_button "検索"
+    save_screenshot "scenario1-3_2_2.png"
+
     # 3.3. 整備開始画面を開く
     click_link "開始"
     save_screenshot "scenario1-3_3_1.png"
@@ -109,9 +130,17 @@ class ScenarioTest < AcceptanceTest
     click_button "開始"
     confirm
     save_screenshot "scenario1-3_3_3.png"
-    # 3.4. 整備作業一覧画面に移動する
+    # 3.4. 整備会社作業一覧画面に移動する
     click_link "戻る"
-    within "tbody > tr" do
+    save_screenshot "scenario1-3_3_3_1.png"
+
+    #※データが多のでエンジン型式とエンジンNo.で検索する
+    select "エンジンモデルコード2", from: "search_engine_model_name"
+    fill_in "search_serialno", with: "SN0001"
+    click_button "検索"
+
+    #ステータスが変更されていることを確認する
+    within "tbody > tr:nth-of-type(1)" do
       assert_equal "整備中", nth_tag(:td, 1).text # ステータス
     end
     save_screenshot "scenario1-3_4.png"
@@ -126,9 +155,14 @@ class ScenarioTest < AcceptanceTest
     # 4.1. サインインする
     sign_in "SG000001", "password"
     save_screenshot "scenario1-4_1.png"
-    # 4.2. 整備作業一覧画面を開く
+    # 4.2. 整備会社作業一覧画面を開く
     click_link "整備会社作業一覧"
     save_screenshot "scenario1-4_2.png"
+    #※データが多のでエンジン型式とエンジンNo.で検索する
+    select "エンジンモデルコード2", from: "search_engine_model_name"
+    fill_in "search_serialno", with: "SN0001"
+    click_button "検索"
+    save_screenshot "scenario1-4_2_2.png"
     # 4.3. 整備完了画面を開く
     click_link "完了"
     save_screenshot "scenario1-4_3_1.png"
@@ -141,11 +175,17 @@ class ScenarioTest < AcceptanceTest
     click_button "完了"
     confirm
     save_screenshot "scenario1-4_3_3.png"
-    # 4.4. 整備作業一覧画面に移動する
+    # 4.4. 整備会社作業一覧画面に移動する
     click_link "戻る"
-    within "tbody > tr" do
-      assert_equal "14/01/09", nth_tag(:td, 6).text # 整備完了日
-      assert_equal "完成品", nth_tag(:td, 1).text    # ステータス
+
+    #※データが多のでエンジン型式とエンジンNo.で検索する
+    select "エンジンモデルコード2", from: "search_engine_model_name"
+    fill_in "search_serialno", with: "SN0001"
+    click_button "検索"
+
+    within "tbody > tr:nth-of-type(1)" do
+      assert_equal "2014/01/09", nth_tag(:td, 6).text # 整備完了日
+      assert_equal "完成品", nth_tag(:td, 1).text     # ステータス
     end
     save_screenshot "scenario1-4_4.png"
     # 4.2. サインアウトする
@@ -166,29 +206,43 @@ class ScenarioTest < AcceptanceTest
     click_link "新規引合"
     save_screenshot "scenario1-5_3_1.png"
     fill_in "物件名", with: "ぶっけんめい"
-    select "西宮戎リペア", from: "engineorder[branch_id]"           # 拠点
-    select "法華倶楽部", from: "engineorder[install_place_id]"      # 設置先
+    fill_in "engineorder_install_place_attributes_postcode", with: "123-4567"
+    fill_in "engineorder_install_place_attributes_address",  with: "大阪市大淀区大淀中大淀町大淀１２３４"
+    fill_in "engineorder_install_place_attributes_phone_no", with: "06-1111-2222"
+    fill_in "engineorder_install_place_attributes_name",     with: "ダミー物件"
+    fill_in "engineorder_install_place_attributes_destination_name",   with: "ご担当者さま"
+    fill_in "engineorder_orderer",   with: "元受株式会社"
+    select "エンジンモデル名1", from: "engineorder_old_engine_attributes_engine_model_name"
+    fill_in "engineorder_old_engine_attributes_serialno",   with: "00000001"
+    select "2000", from: "engineorder_day_of_test_1i" # 試運転日(年)
+    select "12",   from: "engineorder_day_of_test_2i" # 試運転日(月)
+    select "23",   from: "engineorder_day_of_test_3i" # 試運転日(日)
     comment = "ちょっと調子がおかしいので交換してください。"
-    fill_in "コメント(交換理由)", with: comment
+    fill_in "engineorder_change_comment", with: comment
     select "2014", from: "engineorder[desirable_delivery_date(1i)]" # 希望納期(年)
     select "2",    from: "engineorder[desirable_delivery_date(2i)]" # 希望納期(月)
     select "10",   from: "engineorder[desirable_delivery_date(3i)]" # 希望納期(日)
-    select "1GPH00-KS ( 5 )", from: "返却エンジン"
     save_screenshot "scenario1-5_3_2.png"
-    click_button "登録"
+    all(:link_or_button, "引合登録")[1].click  # ボタンが二つあるので一個目をクリックする
     confirm
     save_screenshot "scenario1-5_3_3.png"
+
     # 5.4. 流通情報一覧画面に移動する
-    click_link "戻る"
+    all(:link_or_button, "戻る")[1].click  #「戻る」ボタンが二つあるので一個目をクリックする
+    save_screenshot "scenario1-5_3_3.png"
+
     click_link "メインメニュー"
     click_link "流通情報一覧"
     within "tbody > tr:nth-of-type(1)" do
       assert_equal "引合", nth_tag(:td, 1).text       # ステータス
-      assert_equal "法華倶楽部", nth_tag(:td, 3).text # 設置先
-      assert_equal "14/02/10", nth_tag(:td, 6).text   # 希望納期
+      assert_equal "ぶっけんめい", nth_tag(:td, 2).text       # 物件名
+      assert_equal "ダミー物件", nth_tag(:td, 3).text # 設置先
+      assert_equal "201410-001", nth_tag(:td, 4).text # 発行No
+      assert_equal "2014/02/10", nth_tag(:td, 6).text   # 希望納期
     end
     within "tbody > tr:nth-of-type(2)" do
-      assert_equal "西宮戎リペア", nth_tag(:td, 3).text # 拠点
+      assert_equal "エンジンモデルコード1 (00000001)", nth_tag(:td, 1).text # 返却エンジン
+      assert_equal "YES名古屋営業", nth_tag(:td, 3).text # 拠点(ログインユーザーの属する拠点)
     end
     save_screenshot "scenario1-5_4.png"
     # 5.9. サインアウトする
@@ -209,18 +263,29 @@ class ScenarioTest < AcceptanceTest
     click_link "受注"
     save_screenshot "scenario1-6_3_1.png"
     assert_equal "ぶっけんめい", find_field("物件名").value
-    select "法華倶楽部", from: "engineorder[sending_place_id]" # 送付先
+    fill_in "売上金額（見込）", with: "1000000"
+    fill_in "engineorder_sending_place_attributes_postcode", with: "888-9999"
+    fill_in "engineorder_sending_place_attributes_address",  with: "大阪市大淀区大淀中５６７８"
+    fill_in "engineorder_sending_place_attributes_phone_no", with: "06-333-4444"
+    fill_in "engineorder_sending_place_attributes_name",     with: "ダミー送付先"
+    fill_in "engineorder_sending_place_attributes_destination_name",   with: "送付咲子"
+    comment = "送付先担当の方には連絡済です。"
+    fill_in "engineorder_sending_comment", with: comment
+
     save_screenshot "scenario1-6_3_2.png"
-    click_button "受注登録"
+    all(:link_or_button, "受注登録")[1].click  # ボタンが二つあるので一個目をクリックする
     confirm
     save_screenshot "scenario1-6_3_3.png"
+
     # 6.4. 流通情報一覧画面に移動する
-    click_link "戻る"
+    all(:link_or_button, "戻る")[1].click  #「戻る」ボタンが二つあるので一個目をクリックする
     click_link "メインメニュー"
     click_link "流通情報一覧"
     within "tbody > tr:nth-of-type(1)" do
       assert_equal "受注", nth_tag(:td, 1).text        # ステータス
-      assert_equal "法華倶楽部", nth_tag(:td, 3).text # 送付先
+    end
+    within "tbody > tr:nth-of-type(2)" do
+      assert_equal "ダミー送付先", nth_tag(:td, 4).text # 送付先
     end
     save_screenshot "scenario1-6_4.png"
     # 6.9. サインアウトする
@@ -240,21 +305,24 @@ class ScenarioTest < AcceptanceTest
     # 7.3. 引当登録画面を開く
     click_link "引当"
     save_screenshot "scenario1-7_3_1.png"
-    select "ENG0002 ( SN0001 )", from: "新規エンジン"
+    select "エンジンモデルコード2", from: "engineorder_new_engine_attributes_engine_model_name"
+    select "SN0001", from: "engineorder_new_engine_attributes_serialno"
+    select "西宮戎リペア", from: "select_returning_place"
+
     save_screenshot "scenario1-7_3_2.png"
-    click_button "引当登録"
+    all(:link_or_button, "引当登録")[1].click  # ボタンが二つあるので一個目をクリックする
     confirm
     save_screenshot "scenario1-7_3_3.png"
     # 7.4. 流通情報一覧画面に移動する
-    click_link "戻る"
+    all(:link_or_button, "戻る")[1].click  #「戻る」ボタンが二つあるので一個目をクリックする
     click_link "メインメニュー"
     click_link "流通情報一覧"
     within "table > tbody > tr:nth-of-type(1)" do
       assert_equal "出荷準備中", nth_tag(:td, 1).text # ステータス
     end
     within "table > tbody > tr:nth-of-type(2)" do
-      assert_equal "1GPH00-KS (5)", nth_tag(:td, 1).text # 返却エンジン
-      assert_equal "ENG0002 (SN0001)", nth_tag(:td, 2).text # 新規エンジン
+      assert_equal "エンジンモデルコード1 (00000001)", nth_tag(:td, 1).text # 返却エンジン
+      assert_equal "エンジンモデルコード2 (SN0001)", nth_tag(:td, 2).text # 新規エンジン
     end
     save_screenshot "scenario1-7_4.png"
     # 7.9. サインアウトする
@@ -271,16 +339,25 @@ class ScenarioTest < AcceptanceTest
     # 8.2. 整備会社作業一覧画面を開く
     click_link "整備会社作業一覧"
     save_screenshot "scenario1-8_2.png"
+
+    #※データが多のでエンジン型式とエンジンNo.で検索する
+    select "エンジンモデルコード2", from: "search_engine_model_name"
+    fill_in "search_serialno", with: "SN0001"
+    click_button "検索"
+    save_screenshot "scenario1-8_2_2.png"
+
     # 8.3. 出荷登録画面を開く
     click_link "出荷"
     save_screenshot "scenario1-8_3_1.png"
     fill_in "送り状No（新エンジン）", with: "201312-001-001"
+    comment = "通常便で送ります。"
+    fill_in "出荷コメント", with: comment
     save_screenshot "scenario1-8_3_2.png"
-    click_button "出荷登録"
+    all(:link_or_button, "出荷登録")[1].click  # ボタンが二つあるので一個目をクリックする
     confirm
     save_screenshot "scenario1-8_3_3.png"
     # 8.4. 整備会社作業一覧画面に移動する
-    click_link "戻る"
+    all(:link_or_button, "戻る")[1].click  #「戻る」ボタンが二つあるので一個目をクリックする
     click_link "メインメニュー"
     click_link "整備会社作業一覧"
     save_screenshot "scenario1-8_4.png"
@@ -302,22 +379,49 @@ class ScenarioTest < AcceptanceTest
     click_link "返却"
     save_screenshot "scenario1-9_3_1.png"
     fill_in "送り状No（旧エンジン）", with: "2014-002-015"
-    select "西宮戎リペア", from: "engineorder[returning_place_id]" # 返却先
-    select "2014", from: "engineorder[returning_date(1i)]"         # 返却日(年)
-    select "2月",    from: "engineorder[returning_date(2i)]"       # 返却日(月)
-    select "15",   from: "engineorder[returning_date(3i)]"         # 返却日(日)
+    select "2014", from: "engineorder_returning_date_1i"         # 返却日(年)
+    select "2",    from: "engineorder_returning_date_2i"       # 返却日(月)
+    select "15",   from: "engineorder_returning_date_3i"         # 返却日(日)
     comment = "天地無用でお願いします。"
     fill_in "返却コメント", with: comment                          # 返却コメント
     save_screenshot "scenario1-9_3_2.png"
-    click_button "返却登録"
+    all(:link_or_button, "返却登録")[1].click  # ボタンが二つあるので一個目をクリックする
     confirm
     save_screenshot "scenario1-9_3_3.png"
     # 9.4. 流通情報一覧画面に移動する
-    click_link "戻る"
+    all(:link_or_button, "戻る")[1].click  #「戻る」ボタンが二つあるので一個目をクリックする
     click_link "メインメニュー"
     click_link "流通情報一覧"
     save_screenshot "scenario1-9_4.png"
     # 9.5. サインアウトする
+    sign_out
+
+    ###########################################################################
+    # 10. 返却されたエンジン１を受領する [@SG]
+    ###########################################################################
+    # 10.1. サインインする
+    sign_in "SG000001", "password"
+    save_screenshot "scenario1-10_1.png"
+    # 10.2. エンジン一覧画面を開く
+    click_link "エンジン一覧"
+    save_screenshot "scenario1-10_2.png"
+    # 10.3. 返却エンジンをエンジン型式とエンジンNo.で検索する
+    fill_in "search_engine_model_name", with: "エンジンモデルコード1"
+    fill_in "search_serialno", with: "00000001"
+    click_button "検索"
+    save_screenshot "scenario1-10_3.png"
+
+# 検索のバグが治ったら↓のコードをコメントから復帰させる
+#    within "tbody > tr" do
+#      assert_equal "受領前", nth_tag(:td, 1).text # ステータス
+#      assert_equal "エンジンモデルコード1", nth_tag(:td, 2).text # エンジン型式
+#      assert_equal "00000001", nth_tag(:td, 3).text # エンジンNo.
+#      assert_equal "2014/02/15", nth_tag(:td, 4).text # 返却日
+#      assert_equal "", nth_tag(:td, 5).text # 受領日はブランク
+#      assert_equal "受領", nth_tag(:td, 7).text   # 作業登録は「受領」
+#    end
+
+    # 10.3. サインアウトする
     sign_out
 
   end
